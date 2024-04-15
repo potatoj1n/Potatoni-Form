e43a29a928518b12edc7a12fc73f50646e247af7
potatoj1n <appabomul@gmail.com>
2024-04-15 18:38:59 +0900

    [추가]사용자 응답 페이지 제출 이벤트

--- src/pages/Response.tsx
+++ src/pages/Response.tsx
@@ -1,22 +1,36 @@
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import TitleBox from '../components/Form/TitleBox';
 import PreviewContainer from '../containers/previewContainer';
 import useAppSelector from '../hooks/useAppSelector';
-import { questionActions } from '../reducers/questionReducer';
 
 const Response = () => {
-  const dispatch = useDispatch();
   const form = useAppSelector(state => state.form);
   const { questions } = form;
 
-  const handleReset = () => {
-    dispatch(questionActions.resetAnswer());
+  const handleSubmit = async () => {
+    try {
+      const formData = JSON.stringify({ form });
+      const response = await fetch('https://your-backend-server.com/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: formData,
+      });
+
+      if (response.ok) {
+        alert('응답이 제출되었습니다');
+      } else {
+        throw new Error('서버 응답이 올바르지 않습니다.');
+      }
+    } catch (error) {
+      console.error('제출에 실패했습니다:', error);
+    }
   };
 
   return (
     <div className="bg-blue-200 w-full flex flex-col justify-items-start items-center flex-grow h-screen">
-      <div className="border rounded-2xl mt-20 w-1/2 flex border-t-blue-500 border-t-8 flex-col mb-5 h-auto bg-white p-8">
+      <div className="mt-20 w-1/2 flex flex-col mb-5 h-auto">
         <TitleBox info={form.form} />
       </div>
       <div className="w-1/2 h-auto">
@@ -26,11 +40,10 @@ const Response = () => {
       </div>
       <div className="flex flex-row w-1/2 justify-between items-center">
         <Link to="/">
-          <button className="border-none rounded-md bg-blue-500 text-white w-20 p-3 ml-1">제 출</button>
+          <button onClick={handleSubmit} className="border-none rounded-md bg-blue-500 text-white w-20 p-3 ml-1">
+            제 출
+          </button>
         </Link>
-        <div className="text-blue-500 mr-1" onClick={handleReset}>
-          양식 지우기
-        </div>
       </div>
     </div>
   );
